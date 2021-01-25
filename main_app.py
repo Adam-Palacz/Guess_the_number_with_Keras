5fb69da11be7abf7925fef60852b9dc238189d0e
Adam-Palacz <adam.palacz96@gmail.com>
2021-01-25 19:26:05 +0100

    add error protection

--- main_app.py
+++ main_app.py
@@ -6,6 +6,15 @@ from Guess_the_number_with_Keras.plot_number import plot_number
 model_name = input("Input model name")
 print(train_model(model_name))
 
-number = int(input("Input test number:"))
-print(test_model(number))
-print(plot_number(number))
+try:
+    number = int(input("Input test label number(1-999):"))
+except ValueError as error:
+    print("Wrong input", error)
+else:
+    if number not in range(1, 1000):
+        print("Number not in range")
+    else:
+        print(test_model(number))
+        print(plot_number(number))
+
+
