65c8384910cfbe686661482ce5b57d8eb6850d6b
Adam-Palacz <adam.palacz96@gmail.com>
2021-01-26 18:57:07 +0100

    add test image option and change import path

--- main_app.py
+++ main_app.py
@@ -1,20 +1,26 @@
-from Guess_the_number_with_Keras.train_model import train_model
-from Guess_the_number_with_Keras.test_model import test_model
-from Guess_the_number_with_Keras.plot_number import plot_number
+from train_model import train_model
+from test_model import test_model
+from plot_number import plot_number
+from test_image import test_image
 
 # Use only if you didn't create model
-model_name = input("Input model name")
-print(train_model(model_name))
+# model_name = input("Input model name")
+# print(train_model(model_name))
 
-try:
-    number = int(input("Input test label number(1-999):"))
-except ValueError as error:
-    print("Wrong input", error)
-else:
-    if number not in range(1, 1000):
-        print("Number not in range")
-    else:
-        print(test_model(number))
-        print(plot_number(number))
+# Test model using test dataframe
+# try:
+#     number = int(input("Input test label number(1-999):"))
+# except ValueError as error:
+#     print("Wrong input", error)
+# else:
+#     if number not in range(1, 1000):
+#         print("Number not in range")
+#     else:
+#         print(test_model(number))
+#         print(plot_number(number))
+
+# Test model using painted number in Tkinker
+print(test_image())
+
 
 
