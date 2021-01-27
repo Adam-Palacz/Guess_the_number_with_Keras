ee0721fb7036b1d0f20b652f789933b6a3d23fe5
Adam-Palacz <adam.palacz96@gmail.com>
2021-01-27 21:20:37 +0100

    small build changes

--- paint_number.py
+++ paint_number.py
@@ -4,25 +4,35 @@ from PIL import Image, ImageDraw
 
 def paint_app():
     def save():
-        filename = 'image.jpg'
-        image1.save(filename)
+        filename = f'image.png'
+        image.save(filename)
+
+    def activate_paint(event):
+        global last_x, last_y
+        cv.bind('<B1-Motion>', paint)
+        last_x, last_y = event.x, event.y
 
     def paint(event):
-        x1, y1 = (event.x), (event.y)
-        x2, y2 = (event.x + 1), (event.y + 1)
-        cv.create_oval((x1, y1, x2, y2), fill='black', width=10)
-        draw.line((x1, y1, x2, y2), fill='black', width=10)
+        global last_x, last_y
+        x, y = event.x, event.y
+        cv.create_line((last_x, last_y, x, y), fill='black', width=30, capstyle=ROUND)
+        draw.line((last_x, last_y, x, y), fill='black', width=30)
+        last_x, last_y = x, y
 
     root = Tk()
-    cv = Canvas(root, width=280, height=280, bg='white', highlightthickness=0)
-    image1 = Image.new('RGB', (280, 280), 'white')
-    draw = ImageDraw.Draw(image1)
-    cv.bind('<B1-Motion>', paint)
-    cv.pack(expand=True, fill=BOTH)
+
+    last_x, last_y = None, None
+
+    cv = Canvas(root, width=280, height=280, bg='white')
+    image = Image.new('RGB', (280, 280), 'white')
+    draw = ImageDraw.Draw(image)
+
+    cv.bind('<1>', activate_paint)
+    cv.pack(expand=YES, fill=BOTH)
+
     btn_save = Button(text="save", command=save)
     btn_save.pack()
+
     root.mainloop()
     return "Image saved"
-
-
 
