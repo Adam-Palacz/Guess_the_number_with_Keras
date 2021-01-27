ee1e6e24f779decce94bd31c3d9916ddedd5a130
Adam-Palacz <adam.palacz96@gmail.com>
2021-01-27 21:17:11 +0100

    build small changes

--- plot_number.py
+++ plot_number.py
@@ -4,7 +4,7 @@ from keras.datasets import mnist
 
 def plot_number(number):
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-    img = test_images[number]
-    result = plt.imshow(img, cmap=plt.cm.binary)
+    image = test_images[number]
+    plt.imshow(image, cmap=plt.cm.binary)
     plt.show()
-    return result
+
